c3f567b35dc9640ce52efb8fa6c5456b0f3bc7a8
FaizIsmail55 <faizismail1155@gmail.com>
2026-02-27 09:35:01 +0800

    fix:build:hello_world
    Signed-off-by: FaizIsmail55 <faizismail1155@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 
 /* Future need to display using uart 3 and printf at mainboard */
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <stdbool.h>
 
 /* Create first hello world program */
 #include <stdio.h>
